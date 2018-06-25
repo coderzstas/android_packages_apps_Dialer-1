c9578e3e20c79092809429eef73a886b8b4fafcb
Danny Baumann <dannybaumann@web.de>
2018-06-25 10:47:13 +0200

    Allow per-call account selection.
    Change-Id: Ieac27294f4069b2cff17bb39a0954f74320ad943

--- java/com/android/dialer/app/calllog/CallLogListItemViewHolder.java
+++ java/com/android/dialer/app/calllog/CallLogListItemViewHolder.java
@@ -111,6 +111,7 @@ import java.lang.ref.WeakReference;
  */
 public final class CallLogListItemViewHolder extends RecyclerView.ViewHolder
     implements View.OnClickListener,
+        View.OnLongClickListener,
         MenuItem.OnMenuItemClickListener,
         View.OnCreateContextMenuListener {
 
@@ -311,6 +312,7 @@ public final class CallLogListItemViewHolder extends RecyclerView.ViewHolder
     quickContactView.setOverlay(null);
     quickContactView.setPrioritizedMimeType(Phone.CONTENT_ITEM_TYPE);
     primaryActionButtonView.setOnClickListener(this);
+    primaryActionButtonView.setOnLongClickListener(this);
     primaryActionView.setOnClickListener(this.expandCollapseListener);
     if (this.voicemailPlaybackPresenter != null
         && ConfigProviderComponent.get(this.context)
@@ -524,6 +526,7 @@ public final class CallLogListItemViewHolder extends RecyclerView.ViewHolder
         primaryActionButtonView.setContentDescription(
             TextUtils.expandTemplate(
                 context.getString(R.string.description_voicemail_action), validNameOrNumber));
+        primaryActionButtonView.setTag(null);
         primaryActionButtonView.setVisibility(View.VISIBLE);
       } else {
         primaryActionButtonView.setVisibility(View.GONE);
@@ -1058,7 +1061,8 @@ public final class CallLogListItemViewHolder extends RecyclerView.ViewHolder
       return;
     }
     intentProvider.logInteraction(context);
-    final Intent intent = intentProvider.getIntent(context);
+
+    final Intent intent = intentProvider.getClickIntent(context);
     // See IntentProvider.getCallDetailIntentProvider() for why this may be null.
     if (intent == null) {
       return;
@@ -1088,6 +1092,18 @@ public final class CallLogListItemViewHolder extends RecyclerView.ViewHolder
 
       DialerUtils.startActivityWithErrorToast(context, intent);
     }
+  }
+
+  @Override
+  public boolean onLongClick(View view) {
+    final IntentProvider intentProvider = (IntentProvider) view.getTag();
+    final Intent intent = intentProvider != null
+        ? intentProvider.getLongClickIntent(context) : null;
+    if (intent != null) {
+      DialerUtils.startActivityWithErrorToast(context, intent);
+      return true;
+    }
+    return false;
   }
 
   private static boolean isNonContactEntry(ContactInfo info) {
